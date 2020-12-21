a79c1239f7d10d7619361fb5fe1b3ca8aa7c9583
starliberia <76456453+starliberia@users.noreply.github.com>
2020-12-21 18:59:02 -0500

    Add files via upload
    First upload at 11:57 Dec. 21, 2020

---
+++ js/sticky.js
@@ -0,0 +1,9 @@
+window.onscroll = function(){
+    var header = document.querySelector(".navbar")
+    var sticky = header.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
